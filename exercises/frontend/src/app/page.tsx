1d9046e6e3962364556f64411a32766bef4f56ba
Jed <jedjocson@firstmate.tech>
2025-09-08 13:25:55 +0800

    Remove useEffect for setting fullName.

--- exercises/frontend/src/app/page.tsx
+++ exercises/frontend/src/app/page.tsx
@@ -72,12 +72,8 @@ const NameEditor = (props: {
   } = props;
   const [firstName, setFirstName] = useState<string>(fetchedFirstName || "");
   const [lastName, setLastName] = useState<string>(fetchedLastName || "");
-  const [fullName, setFullName] = useState<string | null>(null);
 
-  useEffect(() => {
-    setFullName(`${firstName} ${lastName}`);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [fetchedFirstName, fetchedLastName]);
+  const fullName = `${fetchedFirstName} ${fetchedLastName}`;
 
   return (
     <div>
